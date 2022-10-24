af43fc81b91cb397559136f9d0b5524fd7294ac9
Lucas Ferreira <lokoporgame194@gmail.com>
2022-10-24 17:48:42 -0300

    Backend do Login
    Feito o Backend do Login de adm, Sistema de sessão,

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.secret_key = 'naruto'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'e-jobs'
+
+mysql = MySQL(app)
 
 from app.controllers import defaults
--- app/controllers/defaults.py
+++ app/controllers/defaults.py
@@ -1,11 +1,40 @@
+from colorama import Cursor
 from flask import Flask, render_template,request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 from app import app
+from app import mysql
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    # Login do Administrador
+    msg = ""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form: 
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM adm_accounts WHERE user_adm = % s And password_adm = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['session_adm'] = True
+            session['id'] = account['id'] 
+            session['username'] = account['user_adm'] 
+            msg = 'Logado com Sucesso!!'
+            return render_template('index.html', msg = msg) 
+        else: 
+            msg = 'Usuário ou Senha Incorretos !!'
+    return render_template('login.html', msg = msg) 
+
+@app.route('/logout') 
+def logout(): 
+   session.pop('session_adm', None) 
+   session.pop('id', None) 
+   session.pop('username', None) 
+   return redirect(url_for('index')) 
+
